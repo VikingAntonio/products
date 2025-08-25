8e49c670f63dc259c75ae7bdc3ecce7855293168
VikingAntonio <77912777+VikingAntonio@users.noreply.github.com>
2025-08-25 01:59:02 -0700

    Add files via upload

---
+++ docs/shirt/assets/js/slider.js
@@ -0,0 +1,10 @@
+// Opcional: detener animación al pasar el mouse
+const track = document.querySelector(".slider-track");
+
+track.addEventListener("mouseenter", () => {
+  track.style.animationPlayState = "paused";
+});
+
+track.addEventListener("mouseleave", () => {
+  track.style.animationPlayState = "running";
+});
